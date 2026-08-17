e4e3f58ad02dbf6b8c139703c7eae296c1eefb77
mahmudtemmy20@gmail.com <mahmudtemmy20@gmail.com>
2026-08-17 18:19:19 +0100

    fix: improve localStorage error message

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -7,7 +7,7 @@ function useLocalStorage<T>(key: string, initialValue: T) {
       const item = window.localStorage.getItem(key);
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
-      console.warn(`Failed to fetch key ${key} from LocalStorage`, error);
+      console.warn(`Fail to fetch key ${key} from LocalStorage`, error);
       return initialValue;
     }
   };
