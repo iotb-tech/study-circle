d29ce5494cce4f0c67ea77d24fb2dbbd9a2ddcba
mahmudtemmy20@gmail.com <mahmudtemmy20@gmail.com>
2026-08-17 18:18:11 +0100

    added to useLocalStorage.ts

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,3 +1,4 @@
+
 import { useState } from "react";
 
 function useLocalStorage<T>(key: string, initialValue: T) {
@@ -9,4 +10,23 @@ function useLocalStorage<T>(key: string, initialValue: T) {
       console.warn(`Failed to fetch key ${key} from LocalStorage`, error);
       return initialValue;
     }
-  };}
+  };
+
+  const [storedValue, setStoredValue] = useState<T>(getStoredValue);
+
+  const setValue = (value: T | ((prevValue: T) => T)) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.warn(`Error setting localStorage key "${key}":`, error);
+    }
+  };
+
+  return [storedValue, setValue] as const;
+}
+
+export default useLocalStorage;
